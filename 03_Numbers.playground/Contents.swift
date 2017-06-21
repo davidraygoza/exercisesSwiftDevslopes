e373cd495991115fd54f8dad5499e40ed9037752
David Raygoza <david.raygoza.ramirez@gmail.com>
2017-06-21 08:42:27 -0500

    03 Number exercise

---
+++ 03_Numbers.playground/Contents.swift
@@ -0,0 +1,32 @@
+//Playground - noun: a place where people can play
+
+import UIKit
+//Numbers
+//Type inference Int
+
+var age = 30
+
+//Explicity declared type
+
+var weight : Int = 200
+
+//Arimethic Operatos
+//+ - / *
+var area = 15 * 20
+
+var sum = 13 + 15
+
+var diff = 10 - 5
+
+var div = 13 / 5
+
+var remainder = 13 % 5
+
+var result = "The result pf 13 / 5 is \(div) with a remainder of \(remainder)"
+
+var randomNumber % 2 == 0{
+    print("this is an even number")
+}else{
+    print("this is an odd number")
+
+}
