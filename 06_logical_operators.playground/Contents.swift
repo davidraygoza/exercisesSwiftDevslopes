de2097ec826218ed0ab0901b71206ccb83233bcf
David Raygoza <david.raygoza.ramirez@gmail.com>
2017-06-23 09:12:33 -0500

    06_Logical_operators

---
+++ 06_logical_operators.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*David Raygoza
+david.raygoza.ramirez@gmail.com
+https://www.linkedin.com/in/davidraygoza/
+*/
+let allowedEntry = false
+if !allowedEntry {
+    print("ACCES DENIED")
+}
+let enteredDoorCode = true
+let passedRetinaScan = false
+let iAmTomCruise = false
+
+if enteredDoorCode && passedRetinaScan|| iAmTomCruise{
+    print("Welcome")
+}else{
+    print("ACCESS DENIED AGAIN")
+}
+
+let hasDoorKey ||knowsOverridePassword{
+    print("Welcome!!"")
+}else{
+    print("YOU STILL AIN'T GETTIN IN FOOL")
+}
